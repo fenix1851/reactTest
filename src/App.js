bacc971dbc0e49ca5a8800ef905d452aa0d3b526
fenix1851 <foxyess@mail.ru>
2022-06-03 19:27:17 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{likes}</h1>
+      <button
+        onClick={() => {
+          setLikes(likes + 1);
+        }}
+      >
+        increment
+      </button>
+      <button
+        onClick={() => {
+          setLikes(likes - 1);
+        }}
+      >
+        decrement
+      </button>
     </div>
   );
 }
